3efb9e45842e1ffac07a34f4621529ac4f657c0f
Kamil Dębiński <kamil.debinski@miquido.com>
2020-01-23 15:10:07 +0100

    Update libraries and cleanup Parcelables

--- app/src/main/java/com/miquido/gistsmvp/models/GistFile.kt
+++ app/src/main/java/com/miquido/gistsmvp/models/GistFile.kt
@@ -1,30 +1,10 @@
 package com.miquido.gistsmvp.models
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-data class GistFile(val filename: String, val content: String?) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(filename)
-        parcel.writeString(content)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<GistFile> {
-        override fun createFromParcel(parcel: Parcel): GistFile {
-            return GistFile(parcel)
-        }
-
-        override fun newArray(size: Int): Array<GistFile?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+@Parcelize
+data class GistFile(
+    val filename: String,
+    val content: String?
+) : Parcelable
--- app/src/main/java/com/miquido/gistsmvp/models/User.kt
+++ app/src/main/java/com/miquido/gistsmvp/models/User.kt
@@ -1,3 +1,8 @@
 package com.miquido.gistsmvp.models
 
-data class User(val name: String, val public_repos: Int, val followers: Int, val html_url: String)
+data class User(
+    val name: String,
+    val public_repos: Int,
+    val followers: Int,
+    val html_url: String
+)
